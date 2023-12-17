a2d1333a7285a9810cc8d438f28bf4207c6d032c
Anjan Shomodder <thatanjan@gmail.com>
2023-12-17 11:50:22 +0600

    Add dialog

---
+++ src/Components/Dialog.jsx
@@ -0,0 +1,41 @@
+'use client'
+import React from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+import '../styles/Dialog.css'
+import DemoForm from './DemoForm'
+
+const DialogDemo = () => (
+	<Dialog.Root>
+		<Dialog.Trigger asChild>
+			<button className='Button violet'>Edit profile</button>
+		</Dialog.Trigger>
+		<Dialog.Portal>
+			<Dialog.Overlay className='DialogOverlay' />
+			<Dialog.Content className='DialogContent'>
+				<Dialog.Title className='DialogTitle'>Edit profile</Dialog.Title>
+				<Dialog.Description className='DialogDescription'>
+					Make changes to your profile here. Click save when you're done.
+				</Dialog.Description>
+				<DemoForm />
+				<div
+					style={{
+						display: 'flex',
+						marginTop: 25,
+						justifyContent: 'space-between',
+					}}
+				>
+					<Dialog.Close asChild>
+						<button className='Close Button' aria-label='Close'>
+							Close
+						</button>
+					</Dialog.Close>
+					<Dialog.Close asChild>
+						<button className='Button green'>Save changes</button>
+					</Dialog.Close>
+				</div>
+			</Dialog.Content>
+		</Dialog.Portal>
+	</Dialog.Root>
+)
+
+export default DialogDemo
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 import DemoForm from '../Components/DemoForm'
+import Accordion from '../Components/Accordion'
+import Dialog from '../Components/Dialog'
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<DemoForm />
+			<Dialog />
+			{/* <Accordion /> */}
 		</main>
 	)
 }
